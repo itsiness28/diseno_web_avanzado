ea99eb255283f1bc69caafc1d985a5a0d18aa846
Inés Castaño Gijón <inescasgi@outlook.com>
2025-09-28 19:57:34 +0200

    Deberes día 28

--- Proyecto_2_Web_Avanzado/code.js
+++ Proyecto_2_Web_Avanzado/code.js
@@ -31,11 +31,33 @@ function Init(){
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
 
-    let rect1 = new Rectangle(10,10,100,50,"red");
-    let rect2 = new Rectangle(300,200,50,50,"blue");
+    rect1 = new Rectangle(10,10,100,50,"red");
+    rect2 = new Rectangle(300,200,50,50,"blue");
 
     rect1.draw(ctx);
     rect2.draw(ctx);
 }
 
+window.addEventListener ("keydown", function (anon)  {
+    var move = 10;
+    switch (anon.key) {
+        case "ArrowUp":
+            rect1.y -= move;
+            break;
+        case "ArrowDown":
+            rect1.y += move;
+            break;
+        case "ArrowLeft":
+            rect1.x -= move;
+            break;
+        case "ArrowRight":
+            rect1.x += move;
+            break;
+    }
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    rect1.draw(ctx);
+    rect2.draw(ctx);
+} )
 window.onload = Init; 
+
